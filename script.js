b969e31fe4a0931aa801c30a8710b20c113adaed
FeliciaIrwin <felicia.irwin@gmail.com>
2014-06-04 18:28:33 -0700

    adding files

---
+++ script.js
@@ -0,0 +1,19 @@
+// <div class="slideshow" data-visible="4" data-pagination="false" data-loop="true" data-transition="crossfade">
+// 	<ul class="carousel">
+// 		<li class="slide"><img src="http://i1320.photobucket.com/albums/u534/Trongard/89b1bc10-adec-455d-9772-0ffbb6858db7_zpsc09b270c.png?t=1394977872" alt="Cthulhu Plushie" />
+// 			Cthulhu Plushie
+// 		</li>
+// 		<li class="slide"><img src="http://i1320.photobucket.com/albums/u534/Trongard/Hats_zpsa23f2fbf.png?t=1394977440" alt="Bookmarks" />
+// 			Hats
+// 		</li>
+// 		<li class="slide"><img src="" alt="" />
+// 			…
+// 		</li>
+// 	</ul>
+// </div>
+
+$(document).ready(function() {
+ 
+  $("#owl-example").owlCarousel();
+ 
+});
